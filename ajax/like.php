93c5cf3b0a15ec4df0527c85fdeab4020795c74e
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-04-20 10:23:09 +0200

    feat: ajax-likes

--- ajax/like.php
+++ ajax/like.php
@@ -1,7 +1,7 @@
 <?php
     require_once("../bootstrap.php");
     if( !empty($_POST) ) {
-        $postId = $_POST['postId'];
+        $postId = $_POST['id'];
         $userId = 1;
 
         $l = new Like();
@@ -9,9 +9,14 @@
         $l->setUserId($userId);
         $l->save();
 
+        $p = new Post();
+        $p->id = $postId;
+        $likes = $p->getLikes();
+
         $result = [
             "status" => "success",
-            "message" => "Like was saved"
+            "message" => "Like was saved",
+            "likes" => $likes
         ];
 
         echo json_encode($result);
--- index.php
+++ index.php
@@ -24,10 +24,40 @@
 	<article>
 		<p><?php echo $post->text; ?></p>
 		<img src="https://picsum.photos/300/200?random=<?php echo rand(1, 10000); ?>" alt="">
-		<div><a href="#" data-id="<?php echo $post->id; ?>" class="like">Like</a> <span class='likes'><?php echo $post->getLikes(); ?></span> people like this </div>
+		<div><a href="#" data-id="<?php echo $post->id; ?>" class="like">Like</a> <span class='likes' id="counter<?php echo $post->id;?>"><?php echo $post->getLikes(); ?></span> people like this </div>
 	</article>
 	<?php endforeach; ?>
+	<script>
+		//add click event to a.like
+		let links = document.querySelectorAll("a.like");
+		for(let i = 0; i < links.length; i++){
+			links[i].addEventListener('click', function(e){
+				e.preventDefault();
+				//console.log("clicked");
+				//get data-id attribute -> de post id mag meegegeven worden maar de user id best via session
+				let id = this.getAttribute("data-id");
+				//console.log(id);
+				//get counter element
+				let counter = document.querySelector("#counter" + id);
+				//fetch request (POST) to like.php, use formdata
+				let formData = new FormData();
+				formData.append("id", id);//append("name", value);
+				fetch("ajax/like.php", {
+					method: "POST",
+					body: formData
+				})
+				.then(function(response){
+					return response.json();
+				})
+				.then(function(json){
+					//console.log(json);
+					//update counter
+					counter.innerHTML = json.likes;
+				})
 
+			});
+		}
+	</script>
 
 
 
